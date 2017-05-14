cb811ec5a21c15fa8dc2a4669b58a2d6418746c4
Mirko Werner <mirko.werner@valiton.com>
2017-05-14 22:32:43 +0200

    axon

--- service.js
+++ service.js
@@ -4,42 +4,44 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const axon = require('axon');
+const socket = axon.socket('pull');
 
 const BotPusher = require('./push_services/bot');
 const BotListPusher = require('./push_services/botlist');
 
+socket.bind(3010);
 
 io.on('connection', function(client) {
 
   let botPusher = new BotPusher(client);
   let botListPusher = new BotListPusher(client);
 
+
   client.on('getBotList', function(data) {
-    console.log('botList required');
     botListPusher.push();
   });
 
-  client.on('newBot', function(data) {
-    console.log('new bot online');
-    console.log(data);
-    botListPusher.push();
+
+  socket.on('message', function(msg){
+
+    let final = JSON.parse(msg.toString());
+
+    switch(final.message) {
+
+      case "pushBotList": {
+        botListPusher.push();
+        break;
+      }
+
+      default: {
+        break;
+      }
+
+    }
   });
 
-  // receiveSocket.on('message', function(msg){
-  //   let final = JSON.parse(msg.toString());
-  //   switch(final.type) {
-  //     case "sendBot": {
-  //       console.log(final.data);
-  //       break;
-  //     }
-  //     default: {
-  //       console.log('defaultCase');
-  //       break;
-  //     }
-  //   }
-  // });
 
-  // botPusher.push('bot1');
 });
 
 http.listen(3001, function(){
